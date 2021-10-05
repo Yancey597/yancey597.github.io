a42d8a4f8e4a5f8ada894e7c6ecc29e660842bed
Yancey597 <704548601@qq.com>
2021-10-06 00:25:27 +0800

    Site updated: 2021-10-06 00:25:26

--- js/matery.js
+++ js/matery.js
@@ -104,7 +104,6 @@ $(function () {
             selector: '.img-item',
             // 启用字幕
             subHtmlSelectorRelative: true
-            showThumbByDefault: false   //这句加上
         });
 
         // progress bar init
